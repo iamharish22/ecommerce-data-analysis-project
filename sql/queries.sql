2798aae11fa1cffb3ee9ad181e7e6219b97b3dbe
iamharish22 <harish.it.17019@recb.ac.in>
2026-05-07 10:20:32 +0530

    Day 3 perform SQL analysis to understand business KPI's and undestand customer behavior

--- sql/queries.sql
+++ sql/queries.sql
@@ -13,7 +13,7 @@ Select count(*) as total_orders
 from orders;
 
 -- Total customers
-select count(distinct(customer_id)) as  total_customers
+select count(distinct(customer_unique_id)) as  total_customers
 from customers;
 
 -- Top 5 customers by revenue
@@ -29,6 +29,27 @@ from orders as o inner join order_payments as p on o.order_id = p.order_id
 group by Months
 order by Months asc;
 
+-- repeat customers
+select customer_id, count(order_id) as total_order
+from orders
+group by customer_id
+having total_order>1;
 
+-- avegrage order values
+select round(sum(p.payment_value)/count(distinct o.order_id),0) as avg_order_value
+from orders as o inner join order_payments as p on o.order_id = p.order_id;
 
+-- Top performing Categories
+select p.product_category_name, count(o.order_id) as total_orders
+from orders as o inner join order_items as i on o.order_id = i.order_id
+inner join products as p on i.product_id = p.product_id 
+group by p.product_category_name
+order by total_orders desc
+limit 5;
 
+-- Revenue by State
+select c.customer_state as state, round(sum(p.payment_value),0) as total_revenue
+from customers as c inner join orders as o on c.customer_id = o.customer_id
+inner join order_payments as p on o.order_id = p.order_id
+group by c.customer_state
+order by total_revenue desc;	
